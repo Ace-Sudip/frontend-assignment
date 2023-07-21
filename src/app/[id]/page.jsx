4e62b438ca35cba1e9aa88b4f95c81ed48ef8bc7
Sudip Tasi <107231650+Ace-Sudip@users.noreply.github.com>
2023-07-22 00:40:57 +0545

    added single route for each product

---
+++ src/app/[id]/page.jsx
@@ -0,0 +1,56 @@
+"use client";
+import { useQuery, useState, useEffect } from "@tanstack/react-query";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+
+function SingledataPage() {
+  const { id } = useParams();
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["data", id],
+    queryFn: () =>
+      fetch(`https://fakestoreapi.com/products/${id}`).then((res) =>
+        res.json()
+      ),
+  });
+
+  if (isLoading || !data) return <div>Loading...</div>;
+  if (isError)
+    return (
+      <div>
+        <Error />
+      </div>
+    );
+
+  console.log(data);
+  return (
+    <div className="container-fluid">
+      <div className="row mt-5 text-center">
+        <div className="col-md-12 ">
+          <div className="card shadow p-3 mb-5 bg-white rounded">
+            <p className="h3">{data.title}</p>
+            <hr />
+            <div className="justify-content-center d-flex">
+              <Image
+                width={200}
+                height={200}
+                src={data.image}
+                alt={data.title}
+                className=" img-fluid resimg mt-3"
+              />
+            </div>
+            <p className="h6 mt-4">Description: {data.description}</p>
+
+            <p className="h5 font-weight-bold">Rating: {data.rating.rate}</p>
+            <p className="h5 font-weight-bold">Price: Rs. {data.price}</p>
+            <button className=" btn m-2 btn-info disabled">Shop Now</button>
+
+            <hr />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SingledataPage;
