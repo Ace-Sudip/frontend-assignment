13c23fbf0dcf42010bf2f76c67c1197aced47cd9
Sudip Tasi <107231650+Ace-Sudip@users.noreply.github.com>
2023-07-22 00:00:14 +0545

    Added react-query

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { ReactQueryProvider } from '@/components/ReactQueryProvier'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReactQueryProvider>
+        {children}
+        </ReactQueryProvider>
+        </body>
     </html>
   )
 }
---
+++ src/components/ReactQueryProvier.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+export const ReactQueryProvider = ({ children }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+        {children}
+        </QueryClientProvider>
+  );
+};
